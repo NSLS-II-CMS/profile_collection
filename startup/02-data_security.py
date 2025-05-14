3c847313a5dd02914838428caf03c54ee65f37e2
CMS Operator <xf11bm@xf11bm-ws1.nsls2.bnl.local>
2025-05-14 09:56:16 -0400

    FIX: DS script

--- startup/02-data_security.py
+++ startup/02-data_security.py
@@ -1,9 +1,9 @@
 # To be deleted when DS is done
 
-RE.md.pop('experiment_alias_directory')
-RE.md.pop('experiment_user')
-RE.md.pop('experiment_SAF_number')
-RE.md.pop('experiment_proposal_number')
-RE.md.pop('experiment_project')
-RE.md.pop('experiment_group')
-RE.md.pop('experiment_cycle')
+RE.md.pop('experiment_alias_directory', None)
+RE.md.pop('experiment_user', None)
+RE.md.pop('experiment_SAF_number', None)
+RE.md.pop('experiment_proposal_number', None)
+RE.md.pop('experiment_project', None)
+RE.md.pop('experiment_group', None)
+RE.md.pop('experiment_cycle', None)
