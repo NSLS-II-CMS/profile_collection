1b532961be20112bf8135331c3f878e6947a3120
CMS Operator <xf11bm@xf11bm-ws1.nsls2.bnl.local>
2025-05-14 09:52:00 -0400

    MNT: setup script for DS

---
+++ startup/02-data_security.py
@@ -0,0 +1,9 @@
+# To be deleted when DS is done
+
+RE.md.pop('experiment_alias_directory')
+RE.md.pop('experiment_user')
+RE.md.pop('experiment_SAF_number')
+RE.md.pop('experiment_proposal_number')
+RE.md.pop('experiment_project')
+RE.md.pop('experiment_group')
+RE.md.pop('experiment_cycle')
